029b93ee08a84b932a1364fbf0c33820afc653dd
lucasrochafortuna-ui <lucasrochafortuna@gmail.com>
2026-05-03 20:58:40 -0300

    training 3.0
    numbers

--- Main/src/com/my/training/formytraining.java
+++ Main/src/com/my/training/formytraining.java
@@ -5,36 +5,25 @@ import java.util.Scanner;
 public class formytraining {
 
 	public static void main(String[] args) {
-		//My training2
+		//trainin3.0
 		Scanner sc = new Scanner(System.in);
 		
-		String c = "sim";
+		int y, x, par, impar;
+		par = 0;
+		impar= 0;
 		do {
-			
-			System.out.println("Informe um intervalo da sua escolha, para digitar uma sequência de numeros");
-			int x = sc.nextInt();
-			int y = sc.nextInt();
-			
-			if (x > y) {
-				System.out.println("repita a sequência");
-				continue;
+			System.out.print("Digite um número: ");
+			x = sc.nextInt();
+			if (x % 2 == 0) {
+				par++;
+			} else {
+				impar++;
 			}
-			
-			int imp = 0;
-			System.out.println("Informe os números que serão contabilizados dentro da sequência: ");
-			while (x <= y) {
-				int z = sc.nextInt();
-				if (z % 2 == 1) {
-					imp += z;
-				}
-				x++;
-			}
-			System.out.print("A soma de todos os números impares digitados no intervalo é: " + imp);
-			System.out.println("");
-			System.out.println("Deseja continuar? [sim/não]");
-			c = sc.next();
-		} while (c.equalsIgnoreCase("sim"));
-			
+			System.out.println("Deseja parar, digite -1:");
+			y = sc.nextInt();
+		} while (y != -1);
+		System.out.println("A quantidade de números impares é: " + impar + " e a quantidade de números pares é: " + par);
+		
 		sc.close();
 	}
 }
