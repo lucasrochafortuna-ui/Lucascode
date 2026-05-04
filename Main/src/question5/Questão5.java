e397222ab839766e0249c65cd0a294f2300d3c6c
lucasrochafortuna-ui <lucasrochafortuna@gmail.com>
2026-05-04 06:16:03 -0300

    question5
    training coe fatorial

---
+++ Main/src/question5/Questão5.java
@@ -0,0 +1,22 @@
+package question5;
+
+import java.util.Scanner;
+
+public class Questão5 {
+
+	public static void main(String[] args) {
+		//training5fatorial
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Informe um número que deseja saber o seu fatorial: ");
+		int x = sc.nextInt();
+		int cont = x;
+		while (x > 1) {
+			x--;
+			cont = cont * x;
+		}
+		System.out.println("O fatorial do numero é: " + cont);
+		sc.close();
+	}
+
+}
