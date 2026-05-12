f0dba68065ba24df8e6dcb8d48086b07519d8190
lucasrochafortuna-ui <lucasrochafortuna@gmail.com>
2026-05-11 21:12:39 -0300

    javastudies
    new question of my list of java

---
+++ Javastudies/src/question13more/Question23.java
@@ -0,0 +1,25 @@
+package question13more;
+
+import java.util.Scanner;
+
+public class Question23 {
+
+	public static void main(String[] args) {
+		// training 23
+		Scanner sc = new Scanner(System.in);
+		
+		char y;
+		do {
+			System.out.println("informe uma temperatura em celsius para saber em fahrenheit");
+			double x = sc.nextDouble();
+			double f = (x * 9) / 5 + 32;
+			System.out.printf("Equivalente em fahrenheit: %.1f%n", f);
+			System.out.println("Deseja continuar? [s/n] ");
+			y = sc.next().charAt(0);
+			
+		} while (y == 's');
+		
+		sc.close();
+	}
+
+}
