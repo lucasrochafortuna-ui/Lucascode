e03d0d547e425ab612d94dc10255023742e7cd11
lucasrochafortuna-ui <lucasrochafortuna@gmail.com>
2026-05-15 16:28:47 -0300

    javastudies
    correção questão de array

--- Javastudies/src/studyArraysInJava/Arrays05.java
+++ Javastudies/src/studyArraysInJava/Arrays05.java
@@ -8,74 +8,48 @@ public class Arrays05 {
 		// training 5
 		Scanner sc = new Scanner(System.in);
 		
-		int[] vector1 = new int [2];
-		int[] vector2 = new int [2];
-		int[] vector3 = new int [4];
-		int cont = 0;
+		System.out.println("Informe o tamanho das duas matrizes que serão utilizadas: ");
+		int[] vector1 = new int [sc.nextInt()];
+		int[] vector2 = new int [sc.nextInt()];
+		int[] vector3 = new int [vector1.length + vector2.length];
 		
-		System.out.println("Informe 10 valores em ordem crescente: ");
+		System.out.println("Informe os valores em ordem crescente do primeiro vetor: ");
 		for (int i = 0 ; i < vector1.length ; i++) {
 			vector1[i] = sc.nextInt();
-			if (vector1[i] > cont) {
-				cont = vector1[i];
-				vector1[i] = cont;
-			} else if (vector1[i] < cont){
-				System.out.println("Número inválido");
-				i--;
-				continue;
-			}else {
-				System.out.println("Números iguais, repita");
-				i--;
-				continue;
-			}
 		}
-		cont = 0;
-		System.out.println("Informe 10 valores em ordem crescente: ");
+		System.out.println("Informe os valores em ordem crescente do segundom vetor: ");
 		for (int i = 0 ; i < vector2.length ; i++) {
 			vector2[i] = sc.nextInt();
-			if (vector2[i] > cont) {
-				cont = vector2[i];
-				vector2[i] = cont;
-			} else if (vector2[i] < cont){
-				System.out.println("Número inválido");
-				i--;
-				continue;
-			} else {
-				System.out.println("Números iguais, repita");
-				i--;
-				continue;
-			}
 		}
-		int j = 0;
-		int i = 0;
-		while (i < vector3.length) {
-			if (vector1[j] < vector2[j]) {
-				vector3[i] = vector1[j];
-				i++;
-				vector3[i] = vector2[j];
-				j++;
-			} else if (vector1[j] > vector2[j]) {
-				vector3[i] = vector2[j];
-				i++;
-				vector3[i] = vector1[j];
-				j++;
-			} else {
-				vector3[i] = vector1[j];
-				i++;
-				vector3[i] = vector2[j];
-				j++;
+		
+		for (int i = 0 ; i < vector1.length ; i++ ) {
+			vector3[i] = vector1[i];
+		}
+		for (int i = 0 ; i < vector2.length ; i++ ) {
+			vector3[vector1.length + i] = vector2[i];
+		}
+		for (int i = 0 ; i < vector3.length - 1; i++) { 
+			// Esse  for serve para rodar o codigo para ajustar tudo, o -1 serve para não contar uma passagem a mais pois pela logica passando n-1 vezes já coloca os número em ordem
+			for (int j = 0 ; j < vector3.length - 1 - i ; j++) {
+				// Esse for ele vai comparar o 1° número e o seu sucessor e assim por diante, o -1 serve para não contabilizar um par a mais assim quebrando o array.
+				// O -i serve para não contabilizar o ultimo número pois o ultimo número já será o maior número digitado ao se compara par a par entre os números.a
+				if (vector3[j] > vector3[j + 1]) {
+					int temp = vector3[j];
+					vector3[j] = vector3[j + 1];
+					vector3[j + 1] = temp;
+				}
 			}
 		}
 		System.out.println("O 1° vetor é: ");
-		for (i = 0 ; i < vector1.length ; i++) {
+		for (int i = 0 ; i < vector1.length ; i++) {
 			System.out.println(vector1[i] + " ");
 		}
 		System.out.println("O 2° vetor é: ");
-		for (i = 0 ; i < vector2.length ; i++) {
+		for (int i = 0 ; i < vector2.length ; i++) {
 			System.out.println(vector2[i] + " ");
 		}
 		System.out.println("O 3° vetor é: ");
-		for (i = 0 ; i < vector3.length ; i++) {
+		for (int i = 0 ; i < vector3.length ; i++) {
 			System.out.println(vector3[i] + " ");
 		}
 		
